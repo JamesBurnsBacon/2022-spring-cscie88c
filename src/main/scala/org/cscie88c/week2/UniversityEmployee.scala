bd737957c3308cdef9c036ea7d0156f4541bce59
JamesBurnsBacon <cookingbaconwithfire@gmail.com>
2022-05-07 21:15:06 -0400

    Final Project

--- src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
+++
@@ -1,7 +0,0 @@
-package org.cscie88c.week2
-
-// write the class UniversityEmployee below
-class UniversityEmployee(val name: String, val email: String) {
-  def description: String =
-    s"Name: ${name}, Email: ${email}"
-}
